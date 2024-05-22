5c0d8c17073815b2e443bcfd3f4071f7e7a09c44
Tenzin Gangshar <thetenzin21@gmail.com>
2024-05-21 22:39:45 -0400

    save as tables in obsidian and added now

--- src/Interview/personalRandom.py
+++ src/Interview/personalRandom.py
@@ -1,6 +1,6 @@
 import random
 import os
-def getQuestion(file:str):
+def getQuestion(file:str) -> list:
    if ".txt" not in file:
       file = file + ".txt"
    questions = []
@@ -8,7 +8,7 @@ def getQuestion(file:str):
       questions = [line for line in file]
    return questions
 
-def ogPrompt(questions):
+def ogPrompt(questions) -> None:
    increment = 0
 
    print("...............input n to exit...............")
@@ -17,28 +17,38 @@ def ogPrompt(questions):
       print(questions[increment])
       increment = increment + 1
       print("next -> ")
-def writeAnswers(questions):
+def writeAnswers(questions) -> None:
    answers = {}
    increment = 0
 
    print(".........press anything to continue.........")
-   while input() != "exit":
-      if increment == 50:
-         print("......HALF WAY THERE......")
-      question = questions[increment].strip() + ": "
-      print(question)
-      answers[increment] = [question, input("Answer: ")]
-      print("exit or next -> ")
 
-      increment = increment + 1
+   while (answer := input().lower()) != "exit":
+      if increment == len(questions)/2:
+            print("......HALF WAY THERE......")
+      if answer == "now":
+         print(increment, " out of ", len(questions))
+         print("Repeating Question now")
+      else:
+         increment = increment + 1
+         question = questions[increment].strip() + ": "
+         print(question)
+         answers[increment] = [question, input("Answer: ")]
+      print("exit or now or next -> ")
+      
    with open('tmp.txt', 'w') as file:
+      file.writelines("|Questions|Answers|\n|...|...|\n")
       for key in answers:
-         file.writelines(answers[key])
+         array = answers[key]
+         table = ["|"] + [val for pair in zip(array, ["|"] * len(array)) for val in pair]
+         file.writelines(table)
          file.write("\n")
+         print(table)
 
 if __name__ == "__main__":
    os.system("dir *.txt")
    file = input("Choose file: ")
    questions = getQuestion(file)
    random.shuffle(questions)
+
    writeAnswers(questions)
